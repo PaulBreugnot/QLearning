5fd422d60a28e102824c6db92f4c3bd86c28ff6d
PaulBreugnot <paul.breugnot@etu.emse.fr>
2017-12-26 14:33:33 +0100

    Improving AbstractReward

--- src/qLearning/model/AbstractReward.java
+++ src/qLearning/model/AbstractReward.java
@@ -6,13 +6,15 @@ public abstract class AbstractReward implements Reward{
 	private StateActionPair stateActionPair;
 	private static Hashtable<StateActionPair, Integer> HashReward = new Hashtable<>();
 	
-
-	
 	protected static void putHashtableValue(StateActionPair stateActionPair, int value) {
 		HashReward.put(stateActionPair, value);
 	}
 	
 	public int getValue() {
 		return HashReward.get(stateActionPair);
+	}
+	
+	public void setValue(StateActionPair stateActionPair) {
+		putHashtableValue(stateActionPair, getReward(stateActionPair));
 	}
 }
