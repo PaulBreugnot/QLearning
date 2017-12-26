25c9d67da4eff1e2b042a47c727bc401af45118f
PaulBreugnot <paul.breugnot@etu.emse.fr>
2017-12-26 11:38:01 +0100

    Adding a new method to implement in State : getAvailableActions.

--- src/qLearning/model/State.java
+++ src/qLearning/model/State.java
@@ -1,11 +1,15 @@
 package qLearning.model;
 
+import java.util.ArrayList;
+
 public interface State {
 	@Override
 	int hashCode();
 	
 	@Override
 	boolean equals(Object stateActionPair);
+	
+	ArrayList<Action> getAvailableActions();
 	
 	boolean isTerminal();
 }
